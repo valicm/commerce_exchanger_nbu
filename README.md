eb439fa0175e454447d65b2c5aa17b0cf27fb31b
Valentino Medimorec <valentino@medjimorec.net>
2020-10-18 13:57:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+CONTENTS OF THIS FILE
+---------------------
+
+* Introduction
+* Requirements
+* Installation
+* Configuration
+* Maintainers
+
+
+INTRODUCTION
+------------
+
+Commerce Exchanger plugin for National Bank of Ukraine (NBU)
+The base currency for exchange rates is UAH.
+
+
+REQUIREMENTS
+------------
+
+This module requires Commerce Exchanger, and enabled UAH as currency.
+
+
+INSTALLATION
+------------
+
+Install the Commerce Exchanger NBU module as you would normally install
+any Drupal contrib module.
+Visit https://www.drupal.org/node/1897420 for further information.
+
+
+CONFIGURATION
+--------------
+
+    1. Navigate to Administration > Extend and enable the Commerce Exchanger
+       module.
+    2. Navigate to Home > Administration > Commerce > Configuration
+                   > Exchange rates.
+
+
+MAINTAINERS
+-----------
+
+The 8.x-1.x branch was created by:
+
+ * Valentino Medimorec (valic) - https://www.drupal.org/u/valic
---
+++ src/Plugin/Commerce/ExchangerProvider/NbuExchanger.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\commerce_exchanger_nbu\Plugin\Commerce\ExchangerProvider;
+
+use Drupal\commerce_exchanger\Plugin\Commerce\ExchangerProvider\ExchangerProviderRemoteBase;
+use Drupal\Component\Serialization\Json;
+
+
+/**
+ * Provides the National Bank of Ukraine exchange rates.
+ *
+ * @CommerceExchangerProvider(
+ *   id = "nbu",
+ *   label = "National Bank of Ukraine",
+ *   display_label = "National Bank of Ukraine",
+ *   historical_rates = TRUE,
+ *   base_currency = "UAH",
+ *   refresh_once = TRUE
+ * )
+ */
+class NbuExchanger extends ExchangerProviderRemoteBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function apiUrl() {
+    return 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRemoteData($base_currency = NULL) {
+    $data = NULL;
+
+    $request = $this->apiClient([]);
+
+    if ($request) {
+      $exchanges = Json::decode($request);
+      foreach ($exchanges as $exchange) {
+        $data[$exchange['cc']] = $exchange['rate'];
+      }
+    }
+
+    return $data;
+  }
+
+}
